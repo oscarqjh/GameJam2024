1a4ca8650aabcf4b8dbbe74c60bf0601a4eb7ffb
oscarqjh <oscar.jh9@gmail.com>
2024-01-28 01:46:17 +0800

    d

--- GameJam2024/src/component/Mic.jsx
+++ GameJam2024/src/component/Mic.jsx
@@ -1,60 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import React, { useEffect, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
-const Mic = ({words, setScore, score, decreaseLife, setSentence}) => {
+const Mic = ({ words, setScore, score, decreaseLife, setSentence }) => {
+  const [no, setNo] = useState(0);
 
-  const [no,setNo] = useState(0)
-  
-const {
+  const {
     transcript,
     listening,
     resetTranscript,
-    browserSupportsSpeechRecognition
+    browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
-  useEffect(()=>{
-    SpeechRecognition.startListening({continuous:true})
-    
-    
-  },[words])
+  useEffect(() => {
+    SpeechRecognition.startListening({ continuous: true });
+  }, [words]);
 
-  useEffect(()=>{
-    setNo(no+1)
-    if(transcript.length>words.length){
-      SpeechRecognition.stopListening()
-      
-      let cnt = 0
-      for(let n=0;n<transcript.split(" ").length;n++){
-        try{
-        if(!words.split(",")[n].includes(transcript.split(" ")[n])){
-          cnt+=1
+  useEffect(() => {
+    setNo(no + 1);
+    if (transcript.length > words.length) {
+      SpeechRecognition.stopListening();
+
+      let cnt = 0;
+      for (let n = 0; n < transcript.split(" ").length; n++) {
+        try {
+          if (!words.split(",")[n].includes(transcript.split(" ")[n])) {
+            cnt += 1;
+          }
+        } catch (e) {
+          resetTranscript();
         }
-      }catch(e){
-        resetTranscript()
-      }
       }
 
-      if(cnt<3){
-        setScore(score+1)
-      }else{
-        decreaseLife()
+      if (cnt < 3) {
+        setScore(score + 1);
+      } else {
+        decreaseLife();
       }
-      
-      setSentence()
-      resetTranscript()
+
+      setSentence();
+      resetTranscript();
       setTimeout(() => {
-        SpeechRecognition.startListening({continuous:true})
+        SpeechRecognition.startListening({ continuous: true });
       }, 1000);
     }
-  },[transcript])
+  }, [transcript]);
 
-
-  return (
-    <>Listening</>
-  );
+  return <>Listening...</>;
 };
 
-  export default Mic;
+export default Mic;
